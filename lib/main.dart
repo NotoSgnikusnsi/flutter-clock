9bce3553b0edda9d62abcb811e625437040d73d8
notosgnikusnsi <notosgnikusnsi@gmail.com>
2024-05-08 14:52:59 +0900

    feat: Show current time.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +13,68 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: "Timer",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 23, 133, 184))),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _now = "";
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), _updateTime);
+  }
+
+  void _updateTime(Timer timer) {
+    DateTime _newCurrentTime = DateTime.now();
+    String _newCurrentTimeString =
+        "${_newCurrentTime.hour}:${_newCurrentTime.minute}:${_newCurrentTime.second.toString().length == 1 ? '0${_newCurrentTime.second}' : _newCurrentTime.second}";
+
+    setState(() {
+      _now = _newCurrentTimeString;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Icon(
+            Icons.timer,
+            color: Theme.of(context).colorScheme.onPrimary,
+          )),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "$_now",
+              style: TextStyle(
+                fontFamily: Theme.of(context).textTheme.titleLarge?.fontFamily,
+                fontWeight: FontWeight.normal,
+                fontSize:
+                    (Theme.of(context).textTheme.titleLarge?.fontSize ?? 14) +
+                        30.0,
+              ),
+            ),
+          ],
         ),
       ),
     );
